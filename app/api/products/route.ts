143eec63740730f8b1f6771d224fee4831b73537
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-04-25 15:52:34 +0300

    feat(api): add products api

---
+++ app/api/products/route.ts
@@ -0,0 +1,22 @@
+import { supabaseFetch } from '@/lib/supabase/fetcher';
+import { NextRequest } from 'next/server';
+
+export async function GET() {
+  const data = await supabaseFetch('products?is_active=eq.true', {
+    next: { revalidate: 60 }, // кеш 60с
+  });
+
+  return Response.json(data);
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  const data = await supabaseFetch('products', {
+    method: 'POST',
+    body,
+    cache: 'no-store',
+  });
+
+  return Response.json(data);
+}
