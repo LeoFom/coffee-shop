2e3966337bfa137879e6d23126c07a1ea9d44b4b
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-04-25 15:55:40 +0300

    feat(dev): update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.qrserver.com',
+        port: '',
+        pathname: '/v1/**',
+      },
+    ],
+    domains: ['api.qrserver.com'],
+  },
 };
 
 export default nextConfig;
