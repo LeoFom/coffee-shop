835ec43e3ae5cb94194b7eff4e793574cfd34118
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-05-11 23:09:23 +0300

    feat(api): add tasks get and patch functions

---
+++ app/api/tasks/route.ts
@@ -0,0 +1,24 @@
+import {nodejsFetch} from "@/lib/nodejs/fetcher";
+import {NextRequest} from "next/server";
+
+export async function GET() {
+  const data = await nodejsFetch(`/tasks`, {
+    method: 'GET',
+    cache: 'no-store',
+  });
+
+  return Response.json(data);
+}
+
+export async function PATCH(req: NextRequest) {
+  const body = await req.json();
+
+  const data = await nodejsFetch(`/tasks`, {
+    method: 'PATCH',
+    body: body,
+    cache: 'no-store',
+  });
+
+  return Response.json(data);
+}
+
