e848f7f6dd155cc759eedb1d623f53c71a0f4e93
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-05-22 09:52:14 +0300

    feat(api): update refresh route

--- app/api/auth/refresh/route.ts
+++ app/api/auth/refresh/route.ts
@@ -2,13 +2,29 @@ import { NextRequest } from 'next/server';
 import {nodejsFetch} from "@/lib/nodejs/fetcher";
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
+  const cookieHeader = req.headers.get('cookie');
 
-  const data = await nodejsFetch(`/auth/refresh`, {
+  const response = await nodejsFetch('/auth/refresh', {
     method: 'POST',
-    body: body,
+    headers: {
+      Cookie: cookieHeader || '',
+    },
+    body: JSON.stringify({}),
     cache: 'no-store',
   });
 
-  return Response.json(data);
+  if (!response.ok) {
+    const errorText = await response.text();
+    console.log("[REFRESH TOKEN] error text -> ",errorText)
+    return new Response(errorText, { status: response.status });
+  }
+
+  const data = await response.json();
+
+  return Response.json(data, {
+    status: response.status,
+    headers: {
+      'set-cookie': response.headers.get('set-cookie') || '',
+    },
+  });
 }
