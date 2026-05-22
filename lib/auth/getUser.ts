e7b480a32ffeb06fd4f2a1e2f56e68f155a39a52
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-05-22 09:46:21 +0300

    feat(lib): update getUser function

--- lib/auth/getUser.ts
+++ lib/auth/getUser.ts
@@ -1,5 +1,13 @@
 import { cookies } from "next/headers";
 
+function extractCookies(setCookie: string) {
+  return setCookie
+    .split(',')
+    .map(cookie => cookie.split(';')[0])
+    .join('; ');
+}
+
+
 export async function getUser() {
   try {
     const cookieStore = await cookies();
@@ -8,58 +16,87 @@ export async function getUser() {
       process.env.NEXT_PUBLIC_API_URL ||
       "http://localhost:3000";
 
+    const initialCookieHeader = cookieStore
+      .getAll()
+      .map((cookie) => `${cookie.name}=${cookie.value}`)
+      .join("; ");
+
+    // 1. Пробуем получить пользователя
     const res = await fetch(
       `${baseUrl}/api/profile/me`,
       {
         method: "GET",
         headers: {
-          Cookie: cookieStore.toString(),
+          cookie: initialCookieHeader,
         },
         cache: "no-store",
       }
     );
 
-    // access token умер
-    if (res.status === 401) {
-
-      const refreshRes = await fetch(
-        `${baseUrl}/api/auth/refresh`,
-        {
-          method: "POST",
-          headers: {
-            Cookie: cookieStore.toString(),
-          },
-        }
-      );
-
-      // refresh тоже умер
-      if (!refreshRes.ok) {
-        return null;
-      }
-
-      // после refresh пробуем снова
-      const retryRes = await fetch(
-        `${baseUrl}/api/profile/me`,
-        {
-          method: "GET",
-          headers: {
-            Cookie: cookieStore.toString(),
-          },
-        }
-      );
-
-      if (!retryRes.ok) {
-        return null;
-      }
-
-      return await retryRes.json();
+    // access token жив
+    if (res.ok) {
+      return await res.json();
     }
 
-    if (!res.ok) {
+    // access token умер
+    if (res.status !== 401) {
       return null;
     }
 
-    return await res.json();
+    // 2. Делаем refresh
+    const refreshRes = await fetch(
+      `${baseUrl}/api/auth/refresh`,
+      {
+        method: "POST",
+        headers: {
+          cookie: initialCookieHeader,
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({}),
+        cache: "no-store",
+      }
+    );
+
+    console.log("!!! refreshRes 3 -> ",refreshRes)
+
+
+    // refresh умер
+    if (!refreshRes.ok) {
+      return null;
+    }
+
+    // 3. Получаем новые cookies
+    const setCookie =
+      refreshRes.headers.get("set-cookie");
+
+    console.log("!!! setCookie 3 -> ",setCookie)
+
+    if (!setCookie) {
+      return null;
+    }
+
+    const cookieHeader = extractCookies(setCookie);
+    console.log(cookieHeader)
+
+    // 4. Повторяем запрос уже с новыми cookies
+    const retryRes = await fetch(
+      `${baseUrl}/api/profile/me`,
+      {
+        method: "GET",
+        headers: {
+          cookie: cookieHeader,
+        },
+        cache: "no-store",
+      }
+    );
+
+    console.log("!!! retryRes 4 -> ",retryRes)
+
+    if (!retryRes.ok) {
+      return null;
+    }
+
+    return await retryRes.json();
 
   } catch (e) {
     console.log(e);
