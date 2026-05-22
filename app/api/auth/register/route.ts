56795ab78f00bca6a377442f8b34f374f761d14b
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-05-22 09:52:21 +0300

    feat(api): update register route

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,14 +1,14 @@
 import { NextRequest } from 'next/server';
-import {nodejsFetch} from "lib/nodejs/fetcher";
+import {nodejsFetch} from "@/lib/nodejs/fetcher";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
   const data = await nodejsFetch(`/auth/register`, {
     method: 'POST',
-    body: body,
+    body: body ? JSON.stringify(body) : undefined,
     cache: 'no-store',
   });
 
-  return Response.json(data);
+  return data;
 }
