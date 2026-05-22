0f67b8046eacb6a14a527f3cb7fae14618f89817
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-05-22 09:53:18 +0300

    feat(api): update profile route

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: NextRequest) {
 
   const data = await nodejsFetch(`/profile`, {
     method: 'POST',
-    body: body,
+    body: body ? JSON.stringify(body) : undefined,
     cache: 'no-store',
   });
 
