0ef962499886c8d9daa32d40d3bb916c458b4f3b
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-05-23 13:51:12 +0300

    feat(components): add setIsAuthenticated from auth store

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -13,8 +13,9 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { useState } from "react";
-import {setAccessToken} from "@/store/auth/authSlice";
+import {setIsAuthenticated} from "@/store/auth/authSlice";
 import {useDispatch} from "react-redux";
+import {useRouter} from "next/navigation";
 
 export function LoginForm({
   className,
@@ -25,6 +26,7 @@ export function LoginForm({
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  const router = useRouter();
   const dispatch = useDispatch()
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -59,7 +61,8 @@ export function LoginForm({
       const data = await response.json();
       console.log("!!! LOG-IN (DATA) -> ",data)
 
-       dispatch(setAccessToken(data.accessToken))
+      dispatch(setIsAuthenticated(true));
+      await router.push("/protected");
       // if (response.ok) {
       //   console.log("Профиль загружен: data", data);
       // }
