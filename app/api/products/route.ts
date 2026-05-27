c7061faa4fb82bab5f1063f7be157c8ccb0f8e7f
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-05-27 23:57:53 +0300

    feat(api): add POST to products route

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,4 +1,5 @@
 import {nodejsFetch} from "@/lib/nodejs/fetcher";
+import {NextRequest} from "next/server";
 
 export async function GET(){
   const data = await nodejsFetch('/products', {
@@ -6,4 +7,30 @@ export async function GET(){
   })
 
   return Response.json(data)
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  const newBody= body ? JSON.stringify(body) : JSON.stringify({})
+
+  const response = await nodejsFetch('/products', {
+    method: 'POST',
+    body: newBody,
+    cache: 'no-store',
+  });
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    return new Response(errorText, { status: response.status });
+  }
+
+  const data = await response.json();
+
+  return Response.json(data, {
+    status: response.status,
+    headers: {
+      'set-cookie': response.headers.get('set-cookie') || '',
+    },
+  });
 }
