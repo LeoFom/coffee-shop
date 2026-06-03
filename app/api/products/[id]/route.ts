66e09345fcfe82df30252d766243c76ad3ee2181
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-06-03 11:01:49 +0300

    feat(api): update /products/[id] GET and PATCH routes

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,40 +1,24 @@
-// import { supabaseFetch } from '@/lib/supabase/fetcher';
-// import { NextRequest } from 'next/server';
-//
-// export async function GET(
-//   _: Request,
-//   { params }: { params: { id: string } }
-// ) {
-//   const data = await supabaseFetch(`products?id=eq.${params.id}`, {
-//     next: { revalidate: 60 },
-//   });
-//
-//   return Response.json(data[0]);
-// }
-//
-// export async function PATCH(
-//   req: NextRequest,
-//   { params }: { params: { id: string } }
-// ) {
-//   const body = await req.json();
-//
-//   const data = await supabaseFetch(`products?id=eq.${params.id}`, {
-//     method: 'PATCH',
-//     body,
-//     cache: 'no-store',
-//   });
-//
-//   return Response.json(data);
-// }
-//
-// export async function DELETE(
-//   _: Request,
-//   { params }: { params: { id: string } }
-// ) {
-//   await supabaseFetch(`products?id=eq.${params.id}`, {
-//     method: 'DELETE',
-//     cache: 'no-store',
-//   });
-//
-//   return Response.json({ success: true });
-// }
+import {nodejsFetch} from "@/lib/nodejs/fetcher";
+import {NextRequest} from "next/server";
+
+export async function GET(id: string){
+  return await nodejsFetch(`/products/${id}`, {
+    method: 'GET'
+  })
+}
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
+  const body = await req.json();
+
+  const response = await nodejsFetch(`/products/${id}`, {
+    method: 'PATCH',
+    body: JSON.stringify(body),
+  });
+
+  return Response.json(await response.json());
+}
