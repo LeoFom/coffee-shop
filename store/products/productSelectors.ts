ae9ffbb3f344a824bbf0e85b4cbbc176a4a2ce76
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-06-06 23:15:52 +0300

    feat(store): update productSelectors

--- store/products/productSelectors.ts
+++ store/products/productSelectors.ts
@@ -1,7 +1,6 @@
 import {RootState} from "@/store/store";
-import {Product} from "@/lib/data/mockProducts";
 
 export const selectProducts = (state: RootState) => state?.products.items;
 
-export const selectProductBySlug = (slug: string) => (state: RootState) =>
-  state?.products.items.find((p: Product) => p.slug === slug);
+// export const selectProductBySlug = (slug: string) => (state: RootState) =>
+//   state?.products.items.find((p: Product) => p.slug === slug);
