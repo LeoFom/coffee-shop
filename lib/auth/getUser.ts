8c99b12df23dfbb0c805b57ec78feb90cc357878
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-06-16 17:27:46 +0300

    feat(lib): update BASE_URL in getUser

--- lib/auth/getUser.ts
+++ lib/auth/getUser.ts
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+const BASE_URL = process.env.NEXT_PUBLIC_URL || "http://localhost:3000";
 
 export async function getUser() {
 
