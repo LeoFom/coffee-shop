a64e9a7d6d5c9954a104ae39f9a2ae510d9bcfbd
Leonid Fomenko <leonidfomenko.ua@gmail.com>
2026-07-21 15:38:30 +0300

    feat(components): add Number to product price in ProductInfo

--- components/shop/product/ProductInfo.tsx
+++ components/shop/product/ProductInfo.tsx
@@ -22,12 +22,12 @@ export default function ProductInfo({ product }: { product: ProductsType }) {
               ${finalPrice.toFixed(2)}
             </span>
             <span className="text-lg text-brand-muted line-through">
-              ${product.price.toFixed(2)}
+              ${Number(product?.price)?.toFixed(2)}
             </span>
           </>
         ) : (
           <span className="text-3xl font-bold text-brand-brown">
-            ${product.price.toFixed(2)}
+            ${Number(product?.price)?.toFixed(2)}
           </span>
         )}
       </div>
